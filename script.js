c5e5c8992867f17e6a8f9ecb45cceccac2d4991e
satongrack <maxwell.satong@gmail.com>
2026-09-21 22:27:57 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-// Banner Slider
+// Banner Slider Auto Play
 let slides = document.querySelectorAll('.slide-item');
 let dots = document.querySelectorAll('.dot');
 let index = 0;
@@ -15,9 +15,10 @@ function autoSlide(){
     if(index > slides.length-1) index=0;
     showSlide(index);
 }
+// 5秒自动轮播
 setInterval(autoSlide,5000);
 
-// Dot Click
+// 点击圆点切换
 dots.forEach((dot,i)=>{
     dot.onclick = function(){
         index = i;
